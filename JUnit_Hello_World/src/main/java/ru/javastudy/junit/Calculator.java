8135f7c9c612274470d19ecaf59ab9a212c041cb
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2018-06-04 22:01:34 +0300

    fifthCommite

--- JUnit_Hello_World/src/main/java/ru/javastudy/junit/Calculator.java
+++ JUnit_Hello_World/src/main/java/ru/javastudy/junit/Calculator.java
@@ -3,15 +3,6 @@ package ru.javastudy.junit;
 public class Calculator {
 
     public int getSum(int x, int y) {
-        return x+y;
+        return x + y;
     }
-
-    public int getDivide(int x, int y) {
-        return x/y;
-    }
-
-    public int getMultiple(int x, int y) {
-        return x*y;
-    }
-
 }
--- JUnit_Hello_World/src/test/java/ru/javastudy/junit/CalculatorTest.java
+++ JUnit_Hello_World/src/test/java/ru/javastudy/junit/CalculatorTest.java
@@ -8,18 +8,6 @@ public class CalculatorTest {
 
     private Calculator calculator;
 
-    @BeforeClass
-    public static void beforeClass() {
-        System.out.println("");
-        System.out.println("Before CalculatorTest.class");
-    }
-
-    @AfterClass
-    public  static void afterClass() {
-        System.out.println("");
-        System.out.println("After CalculatorTest.class");
-    }
-
     @Before
     public void initTest() {
         System.out.println("");
@@ -37,28 +25,5 @@ public class CalculatorTest {
     public void testGetSum() throws Exception {
         System.out.println("testGetSum()");
         assertEquals(15, calculator.getSum(7,8));
-    }
-
-    @Test
-    public void testGetDivide() throws Exception {
-        System.out.println("testGetDivide()");
-        assertEquals(20, calculator.getDivide(100,5));
-    }
-
-    @Test
-    public void testGetMultiple() throws Exception {
-        System.out.println("testGetMultiple()");
-    }
-
-    @Test(expected = ArithmeticException.class)
-    public void divisionWithException() {
-        System.out.println("divisionWithException()");
-        calculator.getDivide(15,0);
-    }
-
-    @Ignore("Message for ignored test")
-    @Test
-    public void ignoredTest() {
-        System.out.println("will not print it");
     }
 }
